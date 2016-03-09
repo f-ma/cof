319a8ef4d0453e5710e16335cdeba5677a578ad9
Huy Pham <fmabroad@gmail.com>
2016-03-09 16:47:47 +0700

    first init

---
+++ test.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var ReactClassEx = React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = ReactClassEx;
